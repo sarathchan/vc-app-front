8e1d9e941ac00510d2d4af3f92e6c519901a9b70
sarath chan <sarathchan20@gmail.com>
2024-03-27 13:30:41 +0530

    s

--- src/SocketContext.js
+++ src/SocketContext.js
@@ -4,9 +4,9 @@ import Peer from 'simple-peer'
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+// const socket = io('http://localhost:5000');
 // const socket = io('http://192.168.1.11:5000');
-// const socket = io('https://va-backend.valuehealthsolutions.com/');
+const socket = io('https://va-backend.valuehealthsolutions.com/');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
